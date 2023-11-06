71366bd883762463bb3cde075aef7f4fde6f8ba2
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-06 18:19:51 +0700

    sample code

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/indexNews";
+import Index from "../src/pages/app";
 import "../src/styles/index.css";
 import Router from "./routes/router";
 import { SidebarProvider } from "./utils/states/sidebarContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Index/>
   </React.StrictMode>
 );
