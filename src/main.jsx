c461537118af500b2f1e25a91744b4658bc19462
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-06 18:19:51 +0700

    sample code

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { SidebarProvider } from "./utils/states/sidebarContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SidebarProvider>
-      <Router />
-    </SidebarProvider>
+    <Index/>
   </React.StrictMode>
 );
