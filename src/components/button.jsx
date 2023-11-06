21405ebb7a6a69a0e67c0e20e9dd4ba41ef6e4ab
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-06 18:19:51 +0700

    sample code

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Button(props) {
   const { label, icon, onClick } = props;
   return (
--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -131,4 +131,4 @@ function App() {
   );
 }
 
-export default App;
+export default Index;
