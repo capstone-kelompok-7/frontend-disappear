eca2bc69104a0481a54e1726cbf7548d7745d3d6
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-06 18:19:51 +0700

    sample code

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
-})
+});
