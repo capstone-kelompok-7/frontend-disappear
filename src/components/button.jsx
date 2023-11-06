8188b4e68b94566ae365ec2bfe096167d505fc0f
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-06 18:19:51 +0700

    sample code

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,13 +1,4 @@
-import React from "react";
-
 export default function Button(props) {
-  const { label, icon, onClick } = props;
-  return (
-    <button onClick={onClick} {...props}>
-      <div className="flex items-center gap-1">
-        <span>{icon}</span>
-        <span>{label}</span>
-      </div>
-    </button>
-  );
+  const { label, className } = props;
+  return <button className={className}>{label}</button>;
 }
