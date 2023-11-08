2e80b5b87b7888897c1d334df33cdd1e13066b51
dianoktaviaa <dian.20061@mhs.unesa.ac.id>
2023-11-08 08:34:51 +0700

    feat: add gap

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -4,7 +4,7 @@ export default function Button(props) {
   const { label, icon, onClick } = props;
   return (
     <button onClick={onClick} {...props}>
-      <div className="flex items-center">
+      <div className="flex items-center gap-1">
         <span>{icon}</span>
         <span>{label}</span>
       </div>
