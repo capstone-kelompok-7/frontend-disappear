5414d1c4a4505bef122322cb41b2100af6742e1a
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-09 17:45:25 +0700

    Add : new input and shadcn

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Layout from "../components/layout";
+import { Input } from "@/components/ui/input";
 
 function App() {
   return (
@@ -7,30 +8,10 @@ function App() {
       <div className="">
         {/* ISI CONTENT DISINI */}
         <h1>ISI CONTENT DISINI</h1>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
-        <div>apa</div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <label htmlFor="picture">Picture</label>
+          <Input id="picture" type="file" />
+        </div>
       </div>
     </Layout>
   );
