2fcc50ef6f111e833503bd8d515341d21d98f8de
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-11-09 11:59:50 +0700

    feat: implement reusable component

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+import DashboardIcon from "../../components/dashboard/dashboardIcon";
+import DashboardDetail from "../../components/dashboard/dashboardDetail";
+
 import IconWallet from "../../assets/icon-wallet.webp";
 import IconBasket from "../../assets/icon-basket.webp";
 import IconUsers from "../../assets/icon-users.webp";
@@ -12,51 +15,32 @@ export default function dashboard() {
       <h1 className="font-bold text-3xl">Dasbor</h1>
 
       <div className="flex gap-10 mt-5">
-        <div className="flex gap-5 items-center shadow-lg py-2 pl-2 pr-5  rounded-lg">
-          <div className="rounded-lg bg-[#D9D9D9] w-16 h-16 p-5 ">
-            <img src={IconWallet} alt="" className="w-10" />
-          </div>
-          <div className="">
-            <p className="text-xl">Pendapatan Terakhir</p>
-            <p className="text-2xl font-bold">Rp 7.000.000</p>
-          </div>
-        </div>
-
-        <div className="flex gap-5 items-center shadow-lg py-2 pl-2 pr-10  rounded-lg">
-          <div className="rounded-lg bg-[#D9D9D9] w-16 h-16 p-5 ">
-            <img src={IconBasket} alt="" className=" w-10" />
-          </div>
-          <div className="">
-            <p className="text-xl">Produk</p>
-            <p className="text-2xl font-bold">20</p>
-          </div>
-        </div>
-
-        <div className="flex gap-5 items-center shadow-lg py-2 pl-2 pr-10  rounded-lg">
-          <div className="rounded-lg bg-[#D9D9D9] w-16 h-16 px-5 py-6 ">
-            <img src={IconUsers} alt="" className="w-10" />
-          </div>
-          <div className="">
-            <p className="text-xl">Pelanggan</p>
-            <p className="text-2xl font-bold">10</p>
-          </div>
-        </div>
-
-        <div className="flex gap-5 items-center shadow-lg py-2 pl-2 pr-10  rounded-lg">
-          <div className="rounded-lg bg-[#D9D9D9] w-16 h-16 p-5 ">
-            <img src={IconNotes} alt="" className="  " />
-          </div>
-          <div className="">
-            <p className="text-xl">Pesanan</p>
-            <p className="text-2xl font-bold">30</p>
-          </div>
-        </div>
+        <DashboardIcon
+          iconSrc={IconWallet}
+          title="Pendapatan Terakhir"
+          value="Rp 7.000.000"
+        />
+        <DashboardIcon iconSrc={IconBasket} title="Produk" value="20" />
+        <DashboardIcon
+          iconSrc={IconUsers}
+          title="Pelanggan"
+          value="10"
+          className="py-6"
+        />
+        <DashboardIcon iconSrc={IconNotes} title="Pesanan" value="30" />
       </div>
 
-      <div className="flex mt-10">
-        <div className="flex">
-          <h1 className="font-boldf">Transaksi Terakhir</h1>
+      <h1 className="font-bold text-2xl mt-10">Transaksi Terakhir</h1>
+      <div className="flex mt-5 ">
+        <div className="flex flex-col">
+          <DashboardDetail
+            name="Dan Asaki"
+            date="Sept 01, 2023"
+            amount="150,000,00"
+            status="Terkonfirmasi"
+          />
         </div>
+        <div>chart</div>
       </div>
     </div>
   );
