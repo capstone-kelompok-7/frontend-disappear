d28e593c605d62adf71a6d9e5d240d990a746080
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-09 19:59:22 +0700

    Add : new Code

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
