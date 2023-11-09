4f9b65801f84a5e0c8fce220fcb2b762a4b7333a
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-09 19:59:22 +0700

    Add : new Code

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,29 @@ module.exports = {
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate"),"flowbite/plugin", require("daisyui")]
 };
