ad236b9c3d583aa011eab41a160499b9bbad542b
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-09 20:02:29 +0700

    Add new code

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Layout from "../components/layout";
 import { Input } from "@/components/ui/input";
+import Button from "@/components/button";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <label htmlFor="picture">Picture</label>
           <Input id="picture" type="file" />
+          <Button type="submit" />
         </div>
       </div>
     </Layout>
