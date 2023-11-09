1e5ca7530e36d3f20c23d1f49c31a5621d5c99c2
albarelfauzan <eggavocados@gmail.com>
2023-11-09 22:05:56 +0700

    feat: penambahan fitur table

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,36 +1,53 @@
 import React from "react";
+import { useTable } from "react-table";
 
-function Tabel({ data, columns }) {
+function Tabel({ columns, data }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+
   return (
-    <table className="min-w-full bg-white shadow-md rounded- border border-neutral-500">
+    <table
+      {...getTableProps()}
+      className="min-w-full bg-white shadow-md rounded- border border-neutral-500"
+    >
       <thead>
-        <tr className="bg-[#C7C7C7] text-black">
-          {columns.map((column, index) => (
-            <th
-              key={index}
-              className="px-6 py-3 text-center font-semibold text-black uppercase tracking-wider border border-neutral-500"
-            >
-              {column}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, rowIndex) => (
+        {headerGroups.map((headerGroup) => (
           <tr
-            key={rowIndex}
-            className={rowIndex % 2 === 0 ? "bg-[#ECECEC]" : "bg-[#FFFFFF]"}
+            {...headerGroup.getHeaderGroupProps()}
+            className="bg-[#C7C7C7] text-black"
           >
-            {columns.map((column, columnIndex) => (
-              <td
-                key={columnIndex}
-                className="px-6 py-4 whitespace-nowrap border border-neutral-500"
+            {headerGroup.headers.map((column) => (
+              <th
+                {...column.getHeaderProps()}
+                className="px-6 py-3 text-center font-semibold text-black uppercase tracking-wider border border-neutral-500"
               >
-                {row[column]}
-              </td>
+                {column.render("Header")}
+              </th>
             ))}
           </tr>
         ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr
+              {...row.getRowProps()}
+              className={row.index % 2 === 0 ? "bg-[#ECECEC]" : "bg-[#FFFFFF]"}
+            >
+              {row.cells.map((cell) => {
+                return (
+                  <td
+                    {...cell.getCellProps()}
+                    className="px-6 py-4 whitespace-nowrap border border-neutral-500"
+                  >
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,21 +1,74 @@
-import { useState } from "react";
+import React from "react";
+import Tabel from "../components/table/table"; // Import komponen tabel yang telah dibuat
 import Layout from "../components/layout";
-import { Input } from "@/components/ui/input";
-import Button from "@/components/button";
 
 function App() {
+  const data = [
+    {
+      Foto: 1,
+      Email: "dimas90@gmail.com",
+      Nama: "John Doe",
+      Telepon: "085343787908",
+      TotalGram: 200,
+      EXP: 1000,
+      Level: "silver",
+    },
+    {
+      Foto: 1,
+      Email: "dimas90@gmail.com",
+      Nama: "John Doe",
+      Telepon: "085343787908",
+      TotalGram: 200,
+      EXP: 1000,
+      Level: "silver",
+    },
+    {
+      Foto: 1,
+      Email: "dimas90@gmail.com",
+      Nama: "John Doe",
+      Telepon: "085343787908",
+      TotalGram: 200,
+      EXP: 1000,
+      Level: "silver",
+    },
+    {
+      Foto: 1,
+      Email: "dimas90@gmail.com",
+      Nama: "John Doe",
+      Telepon: "085343787908",
+      TotalGram: 200,
+      EXP: 1000,
+      Level: "silver",
+    },
+    {
+      Foto: 1,
+      Email: "dimas90@gmail.com",
+      Nama: "John Doe",
+      Telepon: "085343787908",
+      TotalGram: 200,
+      EXP: 1000,
+      Level: "silver",
+    },
+  ];
+
+  const columns = [
+    { Header: "Foto", accessor: "Foto" },
+    { Header: "Email", accessor: "Email" },
+    { Header: "Nama", accessor: "Nama" },
+    { Header: "Telepon", accessor: "Telepon" },
+    { Header: "Total Gram", accessor: "TotalGram" },
+    { Header: "EXP", accessor: "EXP" },
+    { Header: "Level", accessor: "Level" },
+  ];
+
   return (
-    <Layout>
-      <div className="">
-        {/* ISI CONTENT DISINI */}
-        <h1>ISI CONTENT DISINI</h1>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <label htmlFor="picture">Picture</label>
-          <Input id="picture" type="file" />
-          <Button type="submit" />
+    <div className="flex">
+      <Layout>
+        <div className="mt-6 mb-6 mx-10 px-[15px] py-5 shadow-md bg-white rounded-[5px]">
+          <Tabel columns={columns} data={data} />
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </div>
   );
 }
 
