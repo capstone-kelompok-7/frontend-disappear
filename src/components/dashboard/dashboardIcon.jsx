d8c8caf9002e8b8a2cf9df3d6dd3de5870360eff
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-11-09 11:59:32 +0700

    feat: create reusable component section stat dashboard

---
+++ src/components/dashboard/dashboardIcon.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+
+const DashboardIcon = ({ iconSrc, title, value, className }) => {
+  return (
+    <div className="flex gap-5 items-center shadow-lg py-2 pl-2 pr-10 rounded-lg">
+      <div className={`${className} rounded-lg bg-[#D9D9D9] w-16 h-16 p-5`}>
+        <img src={iconSrc} alt="" className="w-10" />
+      </div>
+      <div>
+        <p className="text-xl">{title}</p>
+        <p className="text-2xl font-bold">{value}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardIcon;
