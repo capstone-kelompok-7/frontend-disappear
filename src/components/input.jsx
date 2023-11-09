6c659a44ab9a5ca424ac9f2ce25fe4b34b212f97
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-09 16:37:36 +0700

    complate Input

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,16 +1,40 @@
-import React from "react";
-
 function Input(props) {
-  const { label, className} = props;
+  const { label, type, className, placeholder, hidden } = props;
 
   return (
-    <div className="flex flex-col mb-4">
-      <label className="text-black mb-3">
-        {label}
-      </label>
-      <input className={className} />
+    <div className="flex flex-col mb-4 w-full">
+      <label className="text-black font-bold mb-3">{label}</label>
+      <input
+        className={className}
+        type={type}
+        placeholder={placeholder}
+        hidden={hidden}
+      />
     </div>
   );
 }
 
-export default Input;
+import React from "react";
+
+function Select(props) {
+  const { label, options, value, onChange } = props;
+  return (
+    <div className="flex flex-col mb-4 text-black">
+      <label className="text-black mb-3">{label}</label>
+      <select
+        className="rounded-lg text-black p-1"
+        value={value}
+        onChange={onChange}
+      >
+        {" "}
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export { Input, Select };
