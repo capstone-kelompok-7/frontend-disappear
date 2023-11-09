c6da31d040734bf96730180e8df13c4c2523ab4c
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-09 19:59:22 +0700

    Add : new Code

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,12 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-  },
   resolve: {
     alias: {
-      "@": "/src",
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  define: {
-    global: "globalThis",
-  },
-});
+})
