c31b7e4f0192f012fe2f5a322b56d78e49669d97
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-10 15:40:37 +0700

    Pindah data file and new code

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -138,7 +138,11 @@ export default function Router() {
     },
     {
       path: "/create-news",
-      element: <CreateNews/>,
+      element: <CreateNews />,
+    },
+    {
+      path: "/edit-news",
+      element: <EditNews />,
     },
   ]);
   return <RouterProvider router={router} />;
