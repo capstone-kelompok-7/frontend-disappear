6f6a6f5b5507a64d9499561dba4c0eca9bf66cb6
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-11-10 20:41:50 +0700

    refactor: size bg icon

--- src/components/dashboard/dashboardIcon.jsx
+++ src/components/dashboard/dashboardIcon.jsx
@@ -5,12 +5,12 @@ import React from "react";
 const DashboardIcon = ({ iconSrc, title, value, className }) => {
   return (
     <div className="flex gap-5 items-center shadow-lg py-2 pl-2 pr-10 rounded-lg">
-      <div className={`${className} rounded-lg bg-[#D9D9D9] w-16 h-16 p-5`}>
+      <div className={`${className} rounded-lg bg-[#D9D9D9] w-16 h-16 xl:w-20 xl:h-20 p-5`}>
         <img src={iconSrc} alt="" className="w-10" />
       </div>
       <div>
-        <p className="text-xl">{title}</p>
-        <p className="text-2xl font-bold">{value}</p>
+        <p className="text-xl ">{title}</p>
+        <p className="text-2xl  font-bold">{value}</p>
       </div>
     </div>
   );
