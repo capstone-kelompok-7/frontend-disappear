d2d55f6f9bcfca9c7f1bf2274c1e5956240015d4
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-10 15:40:37 +0700

    Pindah data file and new code

---
+++ src/components/input.jsx
@@ -0,0 +1,40 @@
+function Inputt(props) {
+  const { label, type, className, placeholder, hidden } = props;
+
+  return (
+    <div className="flex flex-col mb-4 w-full">
+      <label className="text-black font-bold mb-3">{label}</label>
+      <input
+        className={className}
+        type={type}
+        placeholder={placeholder}
+        hidden={hidden}
+      />
+    </div>
+  );
+}
+
+import React from "react";
+
+function Select(props) {
+  const { label, options, value, onChange } = props;
+  return (
+    <div className="flex flex-col mb-4 text-black">
+      <label className="text-black mb-3">{label}</label>
+      <select
+        className="rounded-lg text-black p-1"
+        value={value}
+        onChange={onChange}
+      >
+        {" "}
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export { Inputt, Select };
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -118,7 +118,6 @@ export default function Router() {
     {
       path: "/edit-news",
       element: <EditNews />,
-      element: <CreateNews/>,
     },
   ]);
   return <RouterProvider router={router} />;
