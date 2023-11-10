a4a173e222dccf310c9c5efed23e5ab2b60908ff
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-11-10 20:40:22 +0700

    feat: create route to dashboard

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,12 +1,18 @@
-import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import React from "react";
+
 import App from "../pages/App";
+import Dashboard from "../pages/dashboard/dashboard";
 
 export default function Router() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard />,
     },
   ]);
   return <RouterProvider router={router} />;
