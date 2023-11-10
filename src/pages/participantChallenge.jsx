9e7de83c7c9d2597d6778779d77246a1dd305d5d
Raptor-Rap <rmfikri42@gmail.com>
2023-11-10 22:18:39 +0700

    feat: change file name, add calender icon, implement dropdown & breadcrumbs

--- src/pages/participantChallenge.jsx
+++ src/pages/participantChallenge.jsx
@@ -1,26 +1,63 @@
-import { useState } from "react";
-import Layout from "../components/layout";
 import { BiEdit } from "react-icons/bi";
-import { VscTriangleDown } from "react-icons/vsc";
+import { SlCalender } from "react-icons/sl";
+
+import Breadcrumbs from "@/components/breadcrumbs";
+import { Select } from "../components/input";
+import Layout from "@/components/layout";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function App() {
   return (
     <div className="flex">
       <Layout>
-        <div className="mt-6 mb-6 mx-10 px-[15px] py-5 bg-white rounded-[5px] shadow-md flex justify-between">
-          <h1 className="font-bold">Peserta Tantangan</h1>
-          <p>Dashborad / Peserta Tantangan</p>
+        <div className="my-6">
+          <Breadcrumbs pages="Peserta Tantangan" />
         </div>
 
-        <div className="mt-6 mb-6 mx-10 px-[15px] py-5 shadow-md bg-white rounded-[5px]">
-          <div className="flex space-x-3 pb-7">
-            <div className="my-auto">
-              <button className="flex border text-black hover:bg-slate-400 hover:text-white p-4 items-center space-x-40 rounded-lg">
-                <div>Filter</div>
-                <VscTriangleDown />
-              </button>
+        <div className="mx-10 px-[15px] py-5 shadow-md bg-white rounded-[5px]">
+          <div className="flex justify-between items-center mb-7">
+            <div>
+              <p className="font-semibold text-3xl">Semua Peserta</p>
+            </div>
+
+            <div className="flex items-center space-x-3">
+              <div className="flex items-center space-x-3">
+                <p>Data Untuk</p>
+                <SlCalender size={20} />
+                <Select options={["Bulan ini", "Minggu ini", "Hari ini"]} />
+              </div>
+
+              <DropdownMenu>
+                <DropdownMenuTrigger className="flex justify-between items-center rounded-md bg-white py-3 px-3 border gap-20">
+                  <p>Filter</p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    height="5"
+                    viewBox="0 0 10 5"
+                    fill="none"
+                  >
+                    <path
+                      d="M5 4.5L0.669872 0.75L9.33013 0.75L5 4.5Z"
+                      fill="#373737"
+                    />
+                  </svg>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent>
+                  <DropdownMenuItem>Valid</DropdownMenuItem>
+                  <DropdownMenuItem>Tidak Valid</DropdownMenuItem>
+                  <DropdownMenuItem>Menunggu Validasi</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
           </div>
+
           <div className="w-full mx-auto">
             <div className="overflow-x-auto">
               <table className="min-w-full bg-white shadow-md border border-neutral-500">
