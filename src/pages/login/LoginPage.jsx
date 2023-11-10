0853bed6511832fdb4a13833a3c67d8350ec21e5
anandadr5 <anandadwir898@gmail.com>
2023-11-10 22:43:03 +0700

    feat: update login page

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
+import Button from "@/components/button";
 
 const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className="w-full h-full relative overflow-hidden bg-white">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -17,35 +21,40 @@ const LoginPage = () => {
                 >
                   Email
                 </label>
-                <input
+                <Input
                   type="email"
                   name="email"
                   id="email"
-                  className="bg-white border border-black text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="Masukkan email anda"
                   required
                 />
               </div>
-              <div>
+              <div style={{ position: "relative" }}>
                 <label
                   htmlFor="password"
                   className="block mb-2 text-sm font-medium text-gray-900"
                 >
                   Kata Sandi
                 </label>
-                <input
-                  type="password"
+                <Input
+                  type={showPassword ? "text" : "password"}
                   name="password"
                   id="password"
-                  className="bg-white border border-black text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="Masukkan kata sandi anda"
                   required
                 />
                 <svg
+                  onClick={() => setShowPassword(!showPassword)}
                   viewBox="0 0 30 30"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="flex-grow-0 flex-shrink-0 w-[20px] h-[20px] relative"
+                  className="w-5 h-5 absolute right-2 cursor-pointer"
+                  style={{
+                    position: "absolute",
+                    top: "70%",
+                    transform: "translateY(-50%)",
+                    zIndex: 1,
+                  }}
                   preserveAspectRatio="xMidYMid meet"
                 >
                   <path
@@ -54,12 +63,11 @@ const LoginPage = () => {
                   ></path>
                 </svg>
               </div>
-              <button
+              <Button
                 type="submit"
-                className="w-full text-dark bg-[#767676] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-              >
-                Masuk
-              </button>
+                className="w-full text-dark bg-[#767676] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex items-center justify-center"
+                label="Masuk"
+              ></Button>
             </form>
           </div>
         </div>
