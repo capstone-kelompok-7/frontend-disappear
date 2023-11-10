33e43dcf6f445de0a7ba656fd9cb6378716ec8be
Raptor-Rap <rmfikri42@gmail.com>
2023-11-10 22:23:14 +0700

    feat: add some routes

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,14 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import React from "react";
 
-import App from "../pages/App";
 import Dashboard from "../pages/dashboard/dashboard";
 import LoginPage from "../pages/login/LoginPage";
 
+import App from "@/pages/indexChallenge";
+import CreateChallenge from "@/pages/createChallenge/createChallenge";
+import DetailChalenge from "@/pages/detailChallenge/detailChallenge";
+import ParticipantChallange from "@/pages/participantChallenge";
+import EditParticipantChallenge from "@/pages/editParticipantChallenge/editParticipantChallenge";
 
 export default function Router() {
   const router = createBrowserRouter([
@@ -13,13 +17,26 @@ export default function Router() {
       element: <App />,
     },
     {
-
       path: "/dashboard",
       element: <Dashboard />,
     },
     {
       path: "/login",
       element: <LoginPage />,
+      path: "/create-challenge",
+      element: <CreateChallenge />,
+    },
+    {
+      path: "/detail-challenge",
+      element: <DetailChalenge />,
+    },
+    {
+      path: "/participant-challenge",
+      element: <ParticipantChallange />,
+    },
+    {
+      path: "/edit-participant-challenge",
+      element: <EditParticipantChallenge />,
     },
   ]);
   return <RouterProvider router={router} />;
