39215f40dfed5dd8d6ca76cd73f928c40e630ead
Raptor-Rap <rmfikri42@gmail.com>
2023-11-13 20:58:50 +0700

    feat: Add dropdown table component and change name component

--- src/pages/challenge/indexChallenge.jsx
+++ src/pages/challenge/indexChallenge.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Layout from "@/components/layout";
@@ -11,14 +10,10 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import DropdownTable from "@/components/dropdownTable/dropdownTable";
 
-function App() {
+function IndexChallenge() {
   const navigate = useNavigate();
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
 
   return (
     <div className="flex">
@@ -116,50 +111,8 @@ function App() {
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap border border-neutral-500">
                       Belum Kadaluwarsa
-                      <div className="absolute inline-block ml-20">
-                        <div>
-                          <button
-                            type="button"
-                            className="flex px-2 text-gray-600 hover:text-gray-800"
-                            onClick={toggleDropdown}
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="w-4 h-4"
-                              viewBox="0 0 24 24"
-                            >
-                              <path
-                                fill="currentColor"
-                                d="M12 5C13.1046 5 14 5.89543 14 7C14 8.10457 13.1046 9 12 9C10.8954 9 10 8.10457 10 7C10 5.89543 10.8954 5 12 5ZM12 12C10.8954 12 10 12.8954 10 14C10 15.1046 10.8954 16 12 16C13.1046 16 14 15.1046 14 14C14 12.8954 13.1046 12 12 12ZM12 19C10.8954 19 10 19.8954 10 21C10 22.1046 10.8954 23 12 23C13.1046 23 14 22.1046 14 21C14 19.8954 13.1046 19 12 19Z"
-                              ></path>
-                            </svg>
-                          </button>
-                        </div>
-
-                        {isOpen && (
-                          <div className="absolute right-0 mt-2 w-20 rounded-md shadow-lg bg-neutral-400 ring-1 ring-black ring-opacity-5">
-                            <div className="py-1">
-                              <a
-                                href="#"
-                                className="block px-4 py-2 text-sm text-black hover:bg-gray-100"
-                              >
-                                Edit
-                              </a>
-                              <a
-                                href="#"
-                                className="block px-4 py-2 text-sm text-black hover:bg-gray-100"
-                              >
-                                Hapus
-                              </a>
-                              <a
-                                href="#"
-                                className="block px-4 py-2 text-sm text-black hover:bg-gray-100"
-                              >
-                                Lihat
-                              </a>
-                            </div>
-                          </div>
-                        )}
+                      <div className="relative inline-block">
+                        <DropdownTable />
                       </div>
                     </td>
                   </tr>
@@ -181,6 +134,9 @@ function App() {
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap border border-neutral-500">
                       Belum Kadaluwarsa
+                      <div className="relative inline-block">
+                        <DropdownTable />
+                      </div>
                     </td>
                   </tr>
                   <tr className="bg-[#FFFFFF]">
@@ -200,7 +156,10 @@ function App() {
                       100
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap border border-neutral-500">
-                      Kadaluwarsa
+                      Belum Kadaluwarsa
+                      <div className="relative inline-block">
+                        <DropdownTable />
+                      </div>
                     </td>
                   </tr>
                 </tbody>
@@ -213,4 +172,4 @@ function App() {
   );
 }
 
-export default App;
+export default IndexChallenge;
