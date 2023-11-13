1f0c33237310fe42fc74f639d81e5fb0b8e15d18
Raptor-Rap <rmfikri42@gmail.com>
2023-11-13 20:59:58 +0700

    feat: Implement react router dom

--- src/pages/challenge/detailChallenge/detailChallenge.jsx
+++ src/pages/challenge/detailChallenge/detailChallenge.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import Layout from "../../../components/layout";
 import { BsArrowLeft } from "react-icons/bs";
+
+import Layout from "@/components/layout";
+import { Link } from "react-router-dom";
 
 export default function DetailChallange() {
   return (
     <Layout>
       <div className="mt-6 mb-6 mx-6 px-[15px] py-5 flex justify-between">
         <div className="flex items-center font-bold">
-          <BsArrowLeft />
+          <Link to="/">
+            <BsArrowLeft />
+          </Link>
           <p className="pl-2 text-xl">Tantangan</p>
         </div>
       </div>
@@ -15,8 +19,10 @@ export default function DetailChallange() {
       <div className="p-6 mx-[60px] shadow-md bg-white rounded-[5px]">
         <div className="flex justify-between">
           <h1 className="font-bold text-[40px]">Tantangan Menanam Pohon</h1>
-          <div className="bg-zinc-300 rounded-xl py-1 px-6 font-semibold">
-            <p>Belum Kadaluwarsa</p>
+          <div>
+            <div className="bg-zinc-300 rounded-xl py-1 px-6 font-semibold ">
+              <p>Belum Kadaluwarsa</p>
+            </div>
           </div>
         </div>
 
