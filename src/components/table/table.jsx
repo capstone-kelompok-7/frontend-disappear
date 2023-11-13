1f9d01ae1907d1c35efdf89bfec620e2f1efdd09
albarelfauzan <eggavocados@gmail.com>
2023-11-13 21:57:58 +0700

    feat: menambahkan key pada table & table.css

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTable } from "react-table";
+import "../../styles/table.css";
 
 function Tabel({ columns, data }) {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
@@ -11,13 +12,15 @@ function Tabel({ columns, data }) {
       className="min-w-full bg-white shadow-md rounded- border border-neutral-500"
     >
       <thead>
-        {headerGroups.map((headerGroup) => (
+        {headerGroups.map((headerGroup, index) => (
           <tr
+            key={index}
             {...headerGroup.getHeaderGroupProps()}
             className="bg-[#C7C7C7] text-black"
           >
-            {headerGroup.headers.map((column) => (
+            {headerGroup.headers.map((column, columnIndex) => (
               <th
+                key={columnIndex}
                 {...column.getHeaderProps()}
                 className="px-6 py-3 text-center font-semibold text-black uppercase tracking-wider border border-neutral-500"
               >
@@ -28,16 +31,18 @@ function Tabel({ columns, data }) {
         ))}
       </thead>
       <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
+        {rows.map((row, index) => {
           prepareRow(row);
           return (
             <tr
+              key={index}
               {...row.getRowProps()}
               className={row.index % 2 === 0 ? "bg-[#ECECEC]" : "bg-[#FFFFFF]"}
             >
-              {row.cells.map((cell) => {
+              {row.cells.map((cell, cellIndex) => {
                 return (
                   <td
+                    key={cellIndex}
                     {...cell.getCellProps()}
                     className="px-6 py-4 whitespace-nowrap border border-neutral-500"
                   >
