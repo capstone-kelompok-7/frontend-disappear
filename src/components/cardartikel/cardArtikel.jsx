cc57fa7f293da013d78b738c11902d5d19d009f5
Yustinus Galih Purnomo <galihpurnomo654@gmail.com>
2023-11-13 17:18:06 +0700

    add :  new Code

---
+++ src/components/cardartikel/cardArtikel.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+function CardArtikel({ title, content, image_url, created_at }) {
+  const truncateContent = (text, maxWords) => {
+    const words = text.split(" ");
+    return words.length > maxWords
+      ? words.slice(0, maxWords).join(" ") + "..."
+      : text;
+  };
+
+  return (
+    <div className="flex border-y-4 p-4 mt-4 mb-4 shadow-md space-x-8 items-center">
+      <div className="flex-grow">
+        <p>{created_at}</p>
+        <h2 className="text-xl font-bold mb-2">{title}</h2>
+        <p className="text-black">{truncateContent(content, 100)}</p>
+        <div className="flex justify-end p-4">
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <svg
+                width="16"
+                height="4"
+                viewBox="0 0 16 4"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M13 2C13 2.55228 13.4477 3 14 3C14.5523 3 15 2.55228 15 2C15 1.44772 14.5523 1 14 1C13.4477 1 13 1.44772 13 2Z"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M7 2C7 2.55228 7.44772 3 8 3C8.55228 3 9 2.55228 9 2C9 1.44772 8.55228 1 8 1C7.44772 1 7 1.44772 7 2Z"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M1 2C1 2.55228 1.44772 3 2 3C2.55228 3 3 2.55228 3 2C3 1.44772 2.55228 1 2 1C1.44772 1 1 1.44772 1 2Z"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>Edit</DropdownMenuItem>
+              <DropdownMenuItem>Hapus</DropdownMenuItem>
+              <DropdownMenuItem>Lihat</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+      <div className="items-center flex ma">
+        <img
+          className="w-[50rem] h-[15rem] rounded-md object-cover object-center"
+          src={image_url}
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default CardArtikel;
--- src/pages/Artikel/createNews.jsx
+++ src/pages/Artikel/createNews.jsx
@@ -1,55 +1,79 @@
 import React, { useState } from "react";
-import { EditorState, convertToRaw } from "draft-js";
-import { Editor } from "react-draft-wysiwyg";
 import { IoMdCloudUpload } from "react-icons/io";
 import { IoArrowBack } from "react-icons/io5";
-import draftToHtml from "draftjs-to-html";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 import Layout from "../../components/layout";
 import { Inputt } from "../../components/input";
+import TextEditor from "@/components/texteditor/tipTap";
 
 function CreateNews() {
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const [formData, setFormData] = useState({
+    title: "",
+    image: null,
+  });
+  const [uploadedFileName, setUploadedFileName] = useState("");
 
-  const onEditorStateChange = (newEditorState) => {
-    setEditorState(newEditorState);
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+
+    if (name === "image") {
+      const uploadedFile = files[0];
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: uploadedFile,
+      }));
+
+      setUploadedFileName(uploadedFile.name);
+    } else {
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
+  };
+
+  const setContent = (newContent) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      content: newContent,
+    }));
   };
 
   const handleTambahArtikel = () => {
-    // Logika untuk menangani tombol "Tambah Artikel"
-    console.log("Tambah Artikel");
+    console.log("Form Data:", formData);
   };
 
   const handleBatal = () => {
-    // Logika untuk menangani tombol "Batal"
     console.log("Batal");
   };
 
   return (
     <Layout>
-      <form action="">
-        <div className="mx-10 mt-5 text-xl">
+      <form>
+        <div className="mt-5 text-xl">
           <button className="flex items-center space-x-2">
             <IoArrowBack />
             <div className="font-bold">Artikel</div>
           </button>
         </div>
-        <div className="mt-2 mx-20 border-t-2 border-l-2 border-b-2">
+        <div className="mt-5 mx-8 border-t-2 border-l-2 border-b-2">
           <div className="mt-5 ml-5">
             <h1 className="font-bold">Tambah Artikel</h1>
           </div>
           <hr className="border border-black mt-5" />
           <div className="mt-6 ml-4 flex space-x-3">
             <Inputt
-              className="border rounded-sm border-black p-[7px]"
+              className="border rounded-sm border-black p-2"
               type="text"
+              name="title"
               label="Judul"
               placeholder="Judul Artikel"
+              value={formData.title}
+              onChange={handleChange}
             />
             <div className="flex flex-col mb-4 w-full">
               <label className="text-black font-bold mb-3">Unggah File</label>
-              <div className="border p-[7px] rounded-sm border-black">
+              <div className="border p-2 rounded-sm border-black flex">
                 <label
                   htmlFor="fileInput"
                   className="cursor-pointer flex items-center px-2 bg-[#404040] w-[6.5rem] text-white rounded-md"
@@ -59,34 +83,38 @@ function CreateNews() {
                     className="border rounded-sm border-black p-[4px]"
                     type="file"
                     accept="image/*"
+                    name="image"
+                    onChange={handleChange}
                     hidden
                   />
                   <IoMdCloudUpload className="mr-2" />
                   Pilih File
                 </label>
+                {uploadedFileName && (
+                  <p className="text-base ml-2 text-gray-600">
+                    {uploadedFileName}
+                  </p>
+                )}
               </div>
             </div>
           </div>
           <div className="mt-6 ml-4">
             <label className="text-black font-bold">Konten</label>
-            <Editor
-              editorState={editorState}
-              wrapperClassName="demo-wrapper mt-4 overflow-hidden"
-              editorClassName="demo-editor border border-black"
-              onEditorStateChange={onEditorStateChange}
-            />
+            <div>
+              <TextEditor data={formData} setContent={setContent} />
+            </div>
           </div>
-          <div className="mt-20 ml-4 mb-7 flex space-x-3">
+          <div className="mt-6 ml-4 mb-7 flex space-x-3">
             <button
               type="button"
-              className="bg-green-500 text-white px-4 py-2 rounded"
+              className="bg-slate-600 text-white px-4 py-2 rounded"
               onClick={handleTambahArtikel}
             >
               Tambah Artikel
             </button>
             <button
               type="button"
-              className="bg-red-500 text-white px-4 rounded"
+              className="border border-black text-black px-4 rounded"
               onClick={handleBatal}
             >
               Batal
