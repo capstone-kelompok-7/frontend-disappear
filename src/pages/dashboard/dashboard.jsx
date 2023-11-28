db7535e0f4920619c6a08e43568f8c5a6d525a74
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-11-28 17:42:47 +0700

    refactor: dashboard pages wireframe into hifi

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -6,22 +6,26 @@ import React from "react";
 import DashboardDetail from "../../components/dashboard/dashboardDetail";
 import DashboardIcon from "../../components/dashboard/dashboardIcon";
 import Layout from "../../components/layout";
+import Tabel from "@/components/table/table";
 
-import IconWallet from "../../assets/icon-wallet.webp";
-import IconBasket from "../../assets/icon-basket.webp";
-import IconUsers from "../../assets/icon-users.webp";
-import IconNotes from "../../assets/icon-note.webp";
+import IconWallet from "../../assets/icon-wallet.svg";
+import IconBasket from "../../assets/icon-basket.svg";
+import IconUsers from "../../assets/icon-users.svg";
+import IconNotes from "../../assets/icon-note.svg";
+import IconHero from "../../assets/icon-hero.svg";
 
 export default function Dashboard() {
   const data = {
-    labels: ["Mei", "Jun", "Jul", "Aug", "Sep", "Oct"],
+    labels: ["1", "2", "3", "4"],
     datasets: [
       {
-        label: "Total Gram",
-        data: [200, 300, 200, 400, 300, 500],
+        label: "",
+        data: [20, 60, 40, 80, 30],
         fill: false,
-        borderColor: "black",
-        pointBackgroundColor: "white",
+        borderColor: "#25745A",
+        pointBackgroundColor: "#AF8050",
+        tension: "0.4",
+        backgroundColor: "#AF8050",
       },
     ],
   };
@@ -35,13 +39,13 @@ export default function Dashboard() {
         ticks: {
           color: "black",
         },
+        title: {
+          display: true,
+          text: "Minggu",
+        },
       },
       y: {
-        grid: {
-          display: false,
-        },
         ticks: {
-          display: false,
           maxTicksLimit: 6,
         },
         title: {
@@ -51,68 +55,102 @@ export default function Dashboard() {
         beginAtZero: true,
       },
     },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        callbacks: {
+          title: (tooltipItem) => `Minggu Ke- ${tooltipItem[0].label}`,
+          label: (tooltipItem) => `${tooltipItem.formattedValue} gram`,
+        },
+      },
+    },
     maintainAspectRatio: false,
     aspectRatio: 2,
   };
 
+  const chartStyle = {
+    maxHeight: "60vh",
+  };
+  const classNameTr = "bg-blue-700 text-black";
+  const classNameTh =
+    "bg-blue-700 px-6 py-3 text-center font-semibold text-black";
+
+  const transactionData = [
+    {
+      Nama: "Dimas Banyuwangis",
+      Tanggal: "24-11-2023",
+      TotalPembayaran: "Rp.200.000",
+      StatusDashboard: "Konfirmasi",
+    },
+    {
+      Nama: "Dimas Banyuwangis",
+      Tanggal: "24-11-2023",
+      TotalPembayaran: "Rp.200.000",
+      StatusDashboard: "Menunggu Konfirmasi",
+    },
+  ];
+
+  const columns = [
+    { Header: "Nama", accessor: "Nama" },
+    { Header: "Tanggal", accessor: "Tanggal" },
+    { Header: "Total Pembayaran", accessor: "TotalPembayaran" },
+    { Header: "Status", accessor: "StatusDashboard" },
+  ];
+
   return (
     <Layout>
-      <div className=" my-5 h-full">
-        <h1 className="font-bold text-3xl">Dasbor</h1>
-
-        {/* Icon Section */}
-        <div className="flex gap-8 mt-5">
-          <DashboardIcon
-            iconSrc={IconWallet}
-            title="Pendapatan Terakhir"
-            value="Rp 7.000.000"
-          />
-          <DashboardIcon iconSrc={IconBasket} title="Produk" value="20" />
-          <DashboardIcon
-            iconSrc={IconUsers}
-            title="Pelanggan"
-            value="10"
-            className="py-6"
-          />
-          <DashboardIcon iconSrc={IconNotes} title="Pesanan" value="30" />
-        </div>
-
-        {/* Detail Section */}
-        <div className="flex mt-10 ">
-          <div className="">
-            <h1 className="font-bold text-2xl mb-5 ">Transaksi Terakhir</h1>
-            <div className="flex flex-col">
-              <DashboardDetail
-                name="Dan Asaki"
-                date="Sept 01, 2023"
-                amount="150,000,00"
-                status="Terkonfirmasi"
-              />
-              <DashboardDetail
-                name="Dan Asaki"
-                date="Sept 01, 2023"
-                amount="150,000,00"
-                status="Terkonfirmasi"
-              />
-              <DashboardDetail
-                name="Dan Asaki"
-                date="Sept 01, 2023"
-                amount="150,000,00"
-                status="Terkonfirmasi"
-              />
-              <DashboardDetail
-                name="Dan Asaki"
-                date="Sept 01, 2023"
-                amount="150,000,00"
-                status="Terkonfirmasi"
-              />
+      <div className=" w-full h-full">
+        {/* Hero Section */}
+        <div className="flex flex-col bg-[#EFE5DC] relative pb-[10vh] pl-10 rounded-b-2xl">
+          {/* Section 1 - Hero */}
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col gap-3">
+              <h1 className="font-bold text-3xl text-primary-green">
+                Selamat Datang!!!
+              </h1>
+              <p className=" text-primary-green">
+                Saat ini anda berada di halaman dashboard
+              </p>
+            </div>
+            <div>
+              <img src={IconHero} className=" w-5/6" />
             </div>
           </div>
+          {/* Section 2 - Icon Section */}
+          <div className="flex absolute top-1/2 w-full max-w-[70vw] pr-10 mt-6">
+            <div className="flex items-center justify-center w-full gap-8">
+              <DashboardIcon
+                iconSrc={IconWallet}
+                title="Pendapatan Bulan Ini"
+                value="Rp 7.000.000"
+              />
+              <DashboardIcon iconSrc={IconBasket} title="Produk" value="20" />
+              <DashboardIcon iconSrc={IconUsers} title="Pelanggan" value="10" />
+              <DashboardIcon iconSrc={IconNotes} title="Pesanan" value="30" />
+            </div>
+          </div>
+        </div>
 
-          <div className="bg-[#F5F5F5] mx-auto h-[55vh] px-5 pt-4 pb-20">
-            <h1 className="font-bold">Statistik Gram Plastik</h1>
-            <p>Jangka Waktu Jul-Okt 2023</p>
-            <Line data={data} options={options} />
+        {/* Section 3 - Statistic Section */}
+        <div className="px-5 py-6 mt-20 shadow-lg rounded-lg">
+          <h1 className=" font-semibold text-3xl mb-2">
+            Statistik Gram Plastik
+          </h1>
+          <Line
+            data={data}
+            options={options}
+            style={chartStyle}
+            className="bg-[#F4FBF9] rounded-2xl py-10"
+          />
+        </div>
+
+        {/* Section 4 - Transaction Section */}
+        <div className="px-5 py-6 mt-20 shadow-lg rounded-lg">
+          <h1 className="font-semibold text-2xl mb-5 ">Transaksi Terakhir</h1>
+          <div>
+            <Tabel columns={columns} data={transactionData} dashboardTable={true} />
           </div>
         </div>
       </div>
