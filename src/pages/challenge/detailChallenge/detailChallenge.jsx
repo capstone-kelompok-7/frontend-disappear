cd4517a612c664926b36a9c148f330fc9d412187
Raptor-Rap <rmfikri42@gmail.com>
2023-12-02 22:25:06 +0700

    feat: integration to get data

--- src/pages/challenge/detailChallenge/detailChallenge.jsx
+++ src/pages/challenge/detailChallenge/detailChallenge.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { BsArrowLeft } from "react-icons/bs";
+import { Link, useParams } from "react-router-dom";
 
 import Layout from "@/components/layout";
-import { Link } from "react-router-dom";
+import { getDetailChallenge } from "@/utils/api/challenge/challenge/api";
+import { Loading } from "@/components/loading";
 
 export default function DetailChallange() {
+  const [challenge, setChallenge] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    setIsLoading(true);
+    try {
+      setIsLoading(true);
+      const result = await getDetailChallenge(id);
+      setChallenge(result.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const formatDate = (dateString) => {
+    const [day, month, year] = new Date(dateString)
+      .toLocaleDateString("en-GB")
+      .split("/");
+    return `${day}-${month}-${year}`;
+  };
+
   return (
     <Layout>
-      <div className="mt-6 mb-6 mx-6 px-[15px] py-5 flex justify-between">
+      <div className="m-6 px-4  py-5 flex justify-between">
         <div className="flex items-center font-bold">
           <Link to="/tantangan">
             <BsArrowLeft />
@@ -16,48 +46,43 @@ export default function DetailChallange() {
         </div>
       </div>
 
-      <div className="p-6 mx-[60px] rounded-[5px]">
-        <div className="flex justify-between">
-          <h1 className="font-bold text-[40px]">Tantangan Menanam Pohon</h1>
-          <div>
-            <div className="bg-[#82ff80] rounded-xl py-1 px-9 ">
-              <p className="text-black">Belum Kadaluwarsa</p>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="p-6 mx-16 rounded-md">
+          <div className="flex justify-between">
+            <h1 className="font-bold text-[40px]">{challenge.title}</h1>
+            <div>
+              <div className="bg-[#82ff80] rounded-xl py-1 px-9 ">
+                <p className="text-black">{challenge.status}</p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="flex my-6">
-          <div className="justify-center items-center gap-2.5 flex">
-            <p className="font-semibold">Mulai</p>
-            <p className="grow shrink text-xs font-medium">24-10-2023</p>
+          <div className="flex my-6">
+            <div className="justify-center items-center gap-2.5 flex">
+              <p className="font-semibold">Mulai</p>
+              <p className="grow shrink text-xs font-medium">
+                {formatDate(challenge.start_date)}
+              </p>
+            </div>
+            <div className="justify-center items-center gap-2.5 pl-7 flex">
+              <p className="font-semibold">Berakhir</p>
+              <p className="grow shrink text-xs font-medium">
+                {formatDate(challenge.end_date)}
+              </p>
+            </div>
           </div>
-          <div className="justify-center items-center gap-2.5 pl-7 flex">
-            <p className="font-semibold">Berakhir</p>
-            <p className="grow shrink text-xs font-medium">31-10-2023</p>
+          <div className="mb-10">
+            <p>EXP : {challenge.exp}</p>
           </div>
-        </div>
-        <div className="mb-10">
-          <p>EXP : 150</p>
-        </div>
 
-        <div className="w-full h-[230px] bg-zinc-300 my-5 rounded-[5px]"></div>
-        <p className="text-xl">
-          Tanamlah pohon saat kamu berjalan, bermeditasi, atau memasak. Ambil
-          FOTO saat kamu membuatnya. Unggah di Instagram Anda dan beri tag
-          @disappear !!!
-          <br />
-          #SAVEWORLD #SAVETHEEART #BETTERLIFE
-          <br />
-          <br />
-          Mengapa tantangan pohon?
-          <br />
-          Karena Pohon membantu membersihkan udara yang kita hirup, menyaring
-          air yang kita minum dan menyediakan habitat bagi lebih dari 80%
-          keanekaragaman hayati di dunia. Hutan menyediakan lapangan pekerjaan
-          bagi lebih dari 1,6 miliar orang, menyerap karbon berbahaya dari
-          atmosfer, dan merupakan bahan utama dari 25% obat-obatan.
-        </p>
-      </div>
+          <div className="w-full h-60 bg-zinc-300 my-5 rounded-md">
+            <img src={challenge.photo} alt={challenge.photo} />
+          </div>
+          <p className="text-base">{challenge.description}</p>
+        </div>
+      )}
     </Layout>
   );
 }
