a1f534f817f8fb434976683700770d57f6445ff1
Raptor-Rap <rmfikri42@gmail.com>
2023-12-05 19:33:14 +0700

    feat: Add edit challenge features

--- src/utils/api/challenge/challenge/schema.js
+++ src/utils/api/challenge/challenge/schema.js
@@ -3,20 +3,30 @@ import * as z from "zod";
 const MAX_FILE_SIZE = 200000;
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
 
-export const challengeSchema = z.object({
-  challengeStart: z.string().min(1, { message: "Field tidak boleh kosong" }),
+const currentDate = new Date();
+currentDate.setHours(0, 0, 0, 0);
+
+export const ChallengeSchema = z.object({
+  challengeStart: z
+    .string()
+    .min(1, { message: "Field tidak boleh kosong" })
+    .refine((value) => {
+      const startDate = new Date(value);
+      startDate.setHours(0, 0, 0, 0);
+      return startDate >= currentDate;
+    }, "Tanggal mulai harus dari hari ini"),
   challengeEnd: z.string().min(1, { message: "Field tidak boleh kosong" }),
   challengeName: z.string().min(1, { message: "Field tidak boleh kosong" }),
   exp: z.number().min(1, { message: "Field tidak boleh kosong" }),
-  image: z
-    .any()
-    .refine(
-      (files) => files?.[0]?.size <= MAX_FILE_SIZE,
-      `*maksimal 2MB dengan format PNG, JPG, JPEG`
-    )
-    .refine(
-      (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
-      "*format PNG, JPG, JPEG"
-    ),
+  // image: z
+  //   .any()
+  //   .refine(
+  //     (files) => files?.[0]?.size <= MAX_FILE_SIZE,
+  //     `*maksimal 2MB dengan format PNG, JPG, JPEG`
+  //   )
+  //   .refine(
+  //     (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
+  //     "*format PNG, JPG, JPEG"
+  //   ),
   description: z.string().min(1, { message: "Field tidak boleh kosong" }),
 });
