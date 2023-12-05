a8f8f8e67966d6fccedc829cbae030d46fb613da
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-12-05 14:16:29 +0700

    feat: create api config carousel

---
+++ src/utils/api/carousel/api.js
@@ -0,0 +1,50 @@
+import axiosWithConfig from "../axiosWithConfig";
+
+export const getAllCarousel = async (params) => {
+  try {
+    let query = "";
+
+    if (params) {
+      const queryParams = [];
+
+      let key;
+      for (key in params) {
+        queryParams.push(`${key}=${params[key]}`);
+      }
+
+      query = queryParams.join("&");
+    }
+
+    const url = query ? `/api/v1/carousel?${query}` : "/api/v1/carousel";
+    const response = await axiosWithConfig.get(url);
+
+    return response.data;
+  } catch (error) {
+    throw Error(error.response.data.message);
+  }
+};
+
+
+export const createCarousel = async (data) => {
+  try {
+    const newData = {
+      ...data,
+    };
+
+    const response = await axiosWithConfig.post(`api/v1/carousel`, newData);
+
+    return response.data;
+  } catch (error) {
+    throw Error(error.response.data.message);
+  }
+};
+
+export const deleteCarousel = async (id) => {
+  try {
+    const response = await axiosWithConfig.delete(`api/v1/carousel/${id}`);
+
+    return response.data;
+  } catch (error) {
+    throw Error(error.response.data.message);
+  }
+};
