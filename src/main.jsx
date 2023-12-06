f2f251deeadc2a3fa76614940cea48d41fc74fdf
anandadr5 <anandadwir898@gmail.com>
2023-12-06 13:18:45 +0700

    Updated root component to include TokenProvider for token management

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "../src/styles/index.css";
 import Router from "./routes/router";
 import { SidebarProvider } from "./utils/states/sidebarContext";
+import { TokenProvider } from "@/utils/context/TokenContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SidebarProvider>
-      <Router />
-    </SidebarProvider>
+    <TokenProvider>
+      <SidebarProvider>
+        <Router />
+      </SidebarProvider>
+    </TokenProvider>
   </React.StrictMode>
 );
