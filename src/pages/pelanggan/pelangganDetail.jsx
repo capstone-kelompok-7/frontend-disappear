c8235246a40a95acd4ac0aea44097fe473504d42
albarelfauzan <eggavocados@gmail.com>
2023-12-10 23:35:26 +0700

    add loading in pelanggandetail page

--- src/pages/pelanggan/pelangganDetail.jsx
+++ src/pages/pelanggan/pelangganDetail.jsx
@@ -39,7 +39,7 @@ function PelangganDetail() {
           title: (
             <div className="flex items-center">
               <FaRegCheckCircle />
-              <span className="ml-2">Produk berhasil dihapus!</span>
+              <span className="ml-2">Pelanggan berhasil dihapus!</span>
             </div>
           ),
           description:
@@ -52,7 +52,7 @@ function PelangganDetail() {
         title: (
           <div className="flex items-center">
             <CrossCircledIcon />
-            <span className="ml-2">Gagal Menghapus Produk!</span>
+            <span className="ml-2">Gagal Menghapus Pelanggan!</span>
           </div>
         ),
         description: "Terjadi kesalahan saat menghapus pelanggan.",
@@ -89,127 +89,145 @@ function PelangganDetail() {
         <div className="w-[85%] px-5 py-5 m-8 mx-auto shadow-md bg-white font-bold text-lg">
           <h1>Pelanggan</h1>
         </div>
-        <div className="flex">
-          {/* kotak profile pict */}
+        <div>
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <>
+              <div className="flex">
+                {/* kotak profile pict */}
 
-          <div className="flex w-[30%] ml-[8%] border rounded-sm bg-white">
-            <div className="pt-8 pl-6 text-8xl mb-4">
-              <FaUserCircle />
-            </div>
-            <div className="flex flex-col pt-8 pl-8 pr-8 break-all">
-              <p className="font-bold text-xl">{users.name}</p>
-              <p className="text-base mt-[0.2rem]">{users.email}</p>
-              <p className="text-sm mt-[0.2rem] text-[#6E6E6E] ">
-                Login terakhir 15 Agus 23, 10.35 AM
-              </p>
-            </div>
-          </div>
-          {/* kotak exp bar */}
-          <div className="ml-16 w-[50%] mr-[8%] h-38 flex justify-center border rounded-sm bg-white  max-h-36">
-            <div className="w-[85%] pt-14 relative">
-              <Progress progress={progressPercentage} size="md" color="dark" />
-              <div
-                className="absolute top-[15%] text-[#000] font-bold whitespace-nowrap transform translate-x-[-50%]"
-                style={{
-                  left: `calc(${Math.min(progressPercentage, 100)}% - 20px)`,
-                }}
-              >
-                {exp} EXP
-              </div>
-              <span
-                className={`progress-dot ${
-                  progressPercentage > 0 ? "dot-active" : ""
-                }`}
-                style={{ left: "0%" }}
-              ></span>
-              <span
-                className={`progress-dot ${
-                  progressPercentage > 50 ? "dot-active" : ""
-                }`}
-                style={{ left: "49%" }}
-              ></span>
-              <span
-                className={`progress-dot ${
-                  progressPercentage >= 100 ? "dot-active" : ""
-                }`}
-                style={{ left: "99%" }}
-              ></span>
-              <div className="exp-level">
-                <div className="flex justify-between mt-2 absolute w-[100%] font-bold">
-                  <p className="">Bronze</p>
-                  <p className="ml-[-2rem]">Silver</p>
-                  <p className="mr-[-1.5rem]">Gold</p>
+                <div className="flex w-[30%] ml-[8%] border rounded-sm bg-white">
+                  <div className="pt-8 pl-6 text-8xl mb-4">
+                    <FaUserCircle />
+                  </div>
+                  <div className="flex flex-col pt-8 pl-8 pr-8 break-all">
+                    <p className="font-bold text-xl">{users.name}</p>
+                    <p className="text-base mt-[0.2rem]">{users.email}</p>
+                    <p className="text-sm mt-[0.2rem] text-[#6E6E6E] ">
+                      Login terakhir 15 Agus 23, 10.35 AM
+                    </p>
+                  </div>
                 </div>
-                <div className="flex justify-between absolute mt-8 w-[100%] whitespace-nowrap">
-                  <p className="">0-500</p>
-                  <p className="ml-[-1rem]">501-1000</p>
-                  <p className="mr-[-2rem]">1001 ++</p>
+                {/* kotak exp bar */}
+                <div className="ml-16 w-[50%] mr-[8%] h-38 flex justify-center border rounded-sm bg-white  max-h-36">
+                  <div className="w-[85%] pt-14 relative">
+                    <Progress
+                      progress={progressPercentage}
+                      size="md"
+                      color="dark"
+                    />
+                    <div
+                      className="absolute top-[15%] text-[#000] font-bold whitespace-nowrap transform translate-x-[-50%]"
+                      style={{
+                        left: `calc(${Math.min(
+                          progressPercentage,
+                          100
+                        )}% - 20px)`,
+                      }}
+                    >
+                      {exp} EXP
+                    </div>
+                    <span
+                      className={`progress-dot ${
+                        progressPercentage > 0 ? "dot-active" : ""
+                      }`}
+                      style={{ left: "0%" }}
+                    ></span>
+                    <span
+                      className={`progress-dot ${
+                        progressPercentage > 50 ? "dot-active" : ""
+                      }`}
+                      style={{ left: "49%" }}
+                    ></span>
+                    <span
+                      className={`progress-dot ${
+                        progressPercentage >= 100 ? "dot-active" : ""
+                      }`}
+                      style={{ left: "99%" }}
+                    ></span>
+                    <div className="exp-level">
+                      <div className="flex justify-between mt-2 absolute w-[100%] font-bold">
+                        <p className="">Bronze</p>
+                        <p className="ml-[-2rem]">Silver</p>
+                        <p className="mr-[-1.5rem]">Gold</p>
+                      </div>
+                      <div className="flex justify-between absolute mt-8 w-[100%] whitespace-nowrap">
+                        <p className="">0-500</p>
+                        <p className="ml-[-1rem]">501-1000</p>
+                        <p className="mr-[-2rem]">1001 ++</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
 
-        <div className="flex">
-          {/* kotak informasi pelanggan */}
-          <div className="w-[30%] ml-[8%] mt-8 border rounded-sm bg-white">
-            <h1 className="pt-4 pl-6 font-bold text-lg">Informasi Pelanggan</h1>
-            <div className="flex w-full">
-              <div className="flex flex-col w-1/4 pt-6 pl-6 pr-6 gap-8 pb-6">
-                <p className="">Nama</p>
-                <p className="">Email</p>
-                <p className="">Telepon</p>
-                <p className="">Alamat</p>
+              <div className="flex">
+                {/* kotak informasi pelanggan */}
+                <div className="w-[30%] ml-[8%] mt-8 border rounded-sm bg-white">
+                  <h1 className="pt-4 pl-6 font-bold text-lg">
+                    Informasi Pelanggan
+                  </h1>
+                  <div className="flex w-full">
+                    <div className="flex flex-col w-1/4 pt-6 pl-6 pr-6 gap-8 pb-6">
+                      <p className="">Nama</p>
+                      <p className="">Email</p>
+                      <p className="">Telepon</p>
+                      <p className="">Alamat</p>
+                    </div>
+                    <div className="flex flex-col w-3/4 pl-6 pt-6 gap-8 pb-6">
+                      <p className="">{users.name}</p>
+                      <p className=" break-all">{users.email}</p>
+                      <p className="">{users.phone}</p>
+                      <p className="">{users.address}</p>
+                    </div>
+                  </div>
+                </div>
+                {/* aktivitas pelanggan */}
+                <div className="w-[50%] mt-8 mr-[8%] h-[9.5rem] ml-16 rounded-sm bg-white">
+                  <h1 className="text-lg font-bold">Aktivitas Pelanggan</h1>
+                  <div className="flex">
+                    <div className="mt-6 w-[40%] h-[235px] border rounded-sm bg-white">
+                      <p className="flex justify-center mt-14 font-bold text-5xl">
+                        {activity.total_orders}
+                      </p>
+                      <p className="flex justify-center font-medium text-xl">
+                        Pesanan
+                      </p>
+                      <p className="flex justify-center mt-2 font-medium text-xs text-[#787878]">
+                        {activity.num_successful_orders} pesanan sukses
+                      </p>
+                      <p className="flex justify-center mt-1 font-medium text-xs text-[#787878]">
+                        {activity.num_failed_orders} pesanan gagal
+                      </p>
+                    </div>
+                    <div className="ml-8 mt-6 w-[40%] h-[235px] border rounded-sm bg-white">
+                      <p className="flex justify-center mt-14 font-bold text-5xl">
+                        {activity.total_challenges}
+                      </p>
+                      <p className="flex justify-center font-medium text-xl">
+                        Tantangan
+                      </p>
+                      <p className="flex justify-center mt-2 font-medium text-xs text-[#787878]">
+                        {activity.num_successful_challenges} tantangan sukses
+                      </p>
+                      <p className="flex justify-center mt-2 font-medium text-xs text-[#787878]">
+                        {activity.num_failed_challenges} tantangan gagal
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-col w-3/4 pl-6 pt-6 gap-8 pb-6">
-                <p className="">{users.name}</p>
-                <p className=" break-all">{users.email}</p>
-                <p className="">{users.phone}</p>
-                <p className="">{users.address}</p>
+
+              <div className="flex  justify-end relative mt-4 mr-[15.5%] whitespace-nowrap">
+                <Button
+                  onClick={() => handleDelete(users.id)}
+                  label="Hapus Pelanggan"
+                  className="text-white rounded-md bg-black py-3 px-3"
+                />
               </div>
-            </div>
-          </div>
-          {/* aktivitas pelanggan */}
-          <div className="w-[50%] mt-8 mr-[8%] h-[9.5rem] ml-16 rounded-sm bg-white">
-            <h1 className="text-lg font-bold">Aktivitas Pelanggan</h1>
-            <div className="flex">
-              <div className="mt-6 w-[40%] h-[235px] border rounded-sm bg-white">
-                <p className="flex justify-center mt-14 font-bold text-5xl">
-                  {activity.total_orders}
-                </p>
-                <p className="flex justify-center font-medium text-xl">
-                  Pesanan
-                </p>
-                <p className="flex justify-center mt-2 font-medium text-xs text-[#787878]">
-                  {activity.num_successful_orders} pesanan sukses
-                </p>
-                <p className="flex justify-center mt-1 font-medium text-xs text-[#787878]">
-                  {activity.num_failed_orders} pesanan gagal
-                </p>
-              </div>
-              <div className="ml-8 mt-6 w-[40%] h-[235px] border rounded-sm bg-white">
-                <p className="flex justify-center mt-14 font-bold text-5xl">
-                  {activity.total_challenges}
-                </p>
-                <p className="flex justify-center font-medium text-xl">
-                  Tantangan
-                </p>
-                <p className="flex justify-center mt-2 font-medium text-xs text-[#787878]">
-                  {activity.num_successful_challenges} tantangan sukses
-                </p>
-                <p className="flex justify-center mt-2 font-medium text-xs text-[#787878]">
-                  {activity.num_failed_challenges} tantangan gagal
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex  justify-end relative mt-4 mr-[15.5%] whitespace-nowrap">
-          <Button
-            onClick={() => handleDelete(users.id)}
-            label="Hapus Pelanggan"
-            className="text-white rounded-md bg-black py-3 px-3"
-          />
+            </>
+          )}
         </div>
       </div>
     </Layout>
