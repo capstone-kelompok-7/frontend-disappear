d57418d6a58a6ad4a5f891d74e5d11969abb7246
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-12-10 08:44:47 +0700

    refactor: getting id from state selectedId

--- src/pages/carousel/caraouselModal.jsx
+++ src/pages/carousel/caraouselModal.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { useNavigate, useParams } from "react-router-dom";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
-import { CheckCircledIcon } from "@radix-ui/react-icons";
 import { CrossCircledIcon } from "@radix-ui/react-icons";
 import { FaRegCheckCircle } from "react-icons/fa";
 
@@ -20,15 +19,14 @@ import { Loading } from "@/components/loading";
 import { Input } from "@/components/ui/input";
 import Button from "@/components/button";
 
-function PopUp({ handleForceFetch }) {
+function PopUp({ handleForceFetch, selectedId }) {
   const [isLoading, setIsLoading] = useState(false);
-  const [selectedId, setSelectedId] = useState(0);
+  // const [selectedId, setSelectedId] = useState(0);
   const [carousel, setCarousel] = useState([]);
   const [photo, setPhoto] = useState([]);
 
   const navigate = useNavigate();
   const { toast } = useToast();
-  const { id } = useParams();
 
   const {
     reset,
@@ -41,19 +39,19 @@ function PopUp({ handleForceFetch }) {
   });
 
   useEffect(() => {
-    if (id !== undefined) {
+    if (selectedId !== null) {
       fetchData();
     }
-  }, []);
+  }, [selectedId]);
 
   async function fetchData() {
     try {
       setIsLoading(true);
-      const result = await getDetailCarousel(id);
+      const result = await getDetailCarousel(selectedId);
       setCarousel(result.data);
 
       if (result.data) {
-        setSelectedId(result.data.id);
+        // setSelectedId(result.data.id);
         setValue("carouselName", result.data.title);
         // setValue("image", result.data.photo);
         // if (result.data.photo) {
@@ -128,10 +126,10 @@ function PopUp({ handleForceFetch }) {
           </div>
         ),
         description:
-          "Data tantangan carousel diperbarui, nih. Silahkan nikmati fitur lainnya!",
+          "Data carousel diperbarui, nih. Silahkan nikmati fitur lainnya!",
       });
       navigate("/carousel");
-      setSelectedId(0);
+      // setSelectedId(0);
       reset();
     } catch (error) {
       toast({
